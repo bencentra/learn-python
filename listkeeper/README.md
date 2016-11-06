1efcf35d19fadbe0a64ef339a08270434c99765b
Ben Centra <blcentra@gmail.com>
2016-11-05 22:14:51 -0400

    restarting listkeeper, much better this time

---
+++ listkeeper/README.md
@@ -0,0 +1,31 @@
+# listkeeper
+
+A program to manage several lists.
+
+## Requirements
+
+* When the program is run, it lists all `.lst` files in the current directory
+  * If there are no `.lst` files, prompt the user for a name and create a file
+* Print out a list of all lists, such as:
+
+```
+1. list1.lst
+2. list2.lst
+```
+
+* The user is prompted to enter the number of the file to load
+* Load the contents of the list:
+
+```
+1. Milk
+2. Eggs
+3. Whiskey
+```
+
+* If the list is empty, prompt to Add or Quit
+* If the list has at least one item, prompt to Add, Delete, or Quit
+  * If adding, prompt the user for an item to add
+    * When adding, re-sort the list
+  * If deleting, prompt the user for a number to delete
+  * If quitting, prompt to save unsaved changes
+* Also prompt to Save if the list has changes
--- listkeeper/listkeeper.py
+++ listkeeper/listkeeper.py
@@ -3,20 +3,43 @@
 import os
 import sys
 
-# class InvalidInputException(Exception): pass
-
 def main():
     try:
-        files = get_file_list()
+        # Load all ".lst" files, or create one if none exist
+        files = get_files()
         if len(files) == 0:
             print("No lists found!")
-            add_file()
-        else:
-            show_lists(files)
-    except (KeyboardInterrupt):
+            create_list()
+            files = get_files()
+        print_list(files)
+        # Select the file to load
+        index = 0
+        if (len(files) > 1):
+            index = get_int("Load file number: ") - 1
+        items = read_file(files[index])
+        print_list(items)
+        changed = False
+        # Interact with the list
+        while True:
+            action = get_action(items, changed)
+            if action == 'a':
+                print('Add')
+                changed = True
+            elif action == 'd':
+                print('Delete')
+            elif action == 's':
+                print('Save')
+                changed = False
+            elif action == 'q':
+                print('Quit')
+            else:
+                print("Invalid action.")
+    except ValueError as err:
+        print(err)
+    except KeyboardInterrupt:
         quit()
 
-def get_file_list():
+def get_files():
     """
     Returns a list of ".lst" files in the current directory
     """
@@ -24,52 +47,99 @@ def get_file_list():
     list_files = list(filter(lambda x: ".lst" in x, all_files))
     return list_files
 
-# def add_or_quit():
-#     choice = "a"
-#     while True:
-#         try:
-#             choice = str(input("[A]dd or [Q]uit [a]: ")).lower()
-#             if choice not in {"a", "q"}:
-#                 raise InvalidInputException("Invalid input {}".format(choice))
-#             elif choice == "a":
-#                 add_file()
-#                 break
-#             else:
-#                 quit()
-#         except InvalidInputException as iie:
-#             print(iie)
-
-def add_file():
+def create_list():
     """
-    Creates a new file, appending the ".lst" extension if necessary
+    Create a new list file, appending the ".lst" extension if needed
     """
     ext = ".lst"
+    filename = get_string("New list name: ")
+    if ext not in filename:
+        filename += ext
+    write_file(filename)
+
+def print_list(my_list):
+    """
+    Print out a list data structure as an ordered list
+    """
+    try:
+        for i in range(len(my_list)):
+            print("{}. {}".format(str(i + 1), my_list[i]));
+    except TypeError:
+        print("That list is empty!")
+
+def write_file(filename, content=""):
+    """
+    Write a file <filename> with the given <content>
+    """
+    fh = None
+    try:
+        fh = open(filename, "w", encoding="utf8")
+        fh.write(content)
+    except EnvironmentError as err:
+        print("Error writing file:", err)
+    else:
+        print("Saved file")
+    finally:
+        if fh is not None:
+            fh.close()
+
+def read_file(filename):
+    """
+    Open a file and read the contents into a list
+    """
+    fh = None
+    lines = []
+    try:
+        fh = open(filename, "r")
+        for line in fh:
+            lines.append(line.strip())
+    except EnvironmentError as err:
+        print("Error reading file:", err);
+    finally:
+        if fh is not None:
+            fh.close()
+    return sorted(lines)
+
+def get_action(items=[], changed=False):
+    """
+    Get the next command from the user
+    """
+    msg = "[A]dd"
+    if len(items) > 0:
+        msg += ", [D]elete"
+    if changed is not False:
+        msg += ", [S]ave"
+    msg += ", or [Q]uit: "
+    action = get_string(msg)
+    return action.lower()
+
+def get_string(msg):
+    """
+    Get a string input from the user
+    """
     while True:
         try:
-            filename = str(input("New list name: "))
-            if ext not in filename:
-                filename += ext
-            new_file = open(filename, "w", encoding="utf8")
-            new_file.close()
-            print("New list {} created!".format(filename))
-            break;
-        except ValueError:
-            print("Invalid file name.")
-        except OSError:
-            print("Error writing file.")
+            string = str(input(msg))
+            return string
+        except ValueError as err:
+            print("Invalid input:", err)
 
-def show_lists(files):
+def get_int(msg):
     """
-    Print a list of ".lst" files
+    Get an integer input from the user
     """
-    for i in range(len(files)):
-        print("{}: {}".format(str(i + 1), files[i]))
+    while True:
+        try:
+            integer = int(input(msg))
+            return integer
+        except ValueError as err:
+            print("Invalid input:", err)
 
 def quit():
     """
     Exit the program
     """
-    print("\nExiting...")
+    print("\nQuitting...")
     sys.exit()
 
 main()
